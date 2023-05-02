c9b2cced6524b790e989b0f12c55b623171b3264
AK4266 <99728079+AK4266@users.noreply.github.com>
2023-05-02 20:20:39 +0530

    SingleelementinaSortedArray

---
+++ April-2023/SingleelementinaSortedArray.java
@@ -0,0 +1,9 @@
+class Solution {
+    int findOnce(int arr[], int n) {
+        int XOR = 0;
+        for (int i = 0; i < n; i++) {
+            XOR = XOR ^ arr[i];
+        }
+        return XOR;
+    }
+}
