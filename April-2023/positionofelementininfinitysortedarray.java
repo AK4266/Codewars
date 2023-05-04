725ecc9b456fc1ab261f03e23d8c8998c3f31fa4
AK4266 <99728079+AK4266@users.noreply.github.com>
2023-05-04 18:57:53 +0530

    positionofelementininfinitysortedarray

---
+++ April-2023/positionofelementininfinitysortedarray.java
@@ -0,0 +1,32 @@
+public class positionofelementininfinitysortedarray {
+    public static int binarySearch(int[] arr, int start, int end, int target) {
+        int mid = 0;
+        while (start <= end)
+            mid = start + (end - start) / 2;
+        if (target < arr[mid]) {
+            end = mid - 1;
+        } else if (target > arr[mid]) {
+            start = mid + 1;
+        } else {
+            return mid;
+        }
+        return -1;
+    }
+
+    public static int findIndex(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+        while (arr[end] < target) {
+            start = end;
+            end = end * 2;
+        }
+        return binarySearch(arr, start, end, target);
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170 };
+        int ans = findIndex(arr, 130);
+        System.out.print(ans);
+    }
+}
